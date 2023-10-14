ae1a41d3be6ab5398e8d06b08a8a121e37bac5d1
Asnoyon <blackpain1100@gmail.com>
2023-10-14 21:35:00 +0600

    file created for employee

---
+++ src/Page/Dashboard/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Swal from "sweetalert2";
+import Header from "./Header";
+import List from "./List";
+import Add from "./Add";
+import Edit from "./Edit";
+import { employessData } from "../../data";
+function Dashboard() {
+
+  return <div>index</div>;
+}
+
+export default Dashboard;

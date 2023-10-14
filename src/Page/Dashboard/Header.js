3eb6817f81898a15e5ca1ce123e353e6a75baaa7
Asnoyon <blackpain1100@gmail.com>
2023-10-14 23:47:48 +0600

    list rendering for employee

--- src/Page/Dashboard/Header.js
+++ src/Page/Dashboard/Header.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-function Header() {
+function Header({setIsAdding}) {
   return (
-    <div>Header</div>
+    <header>
+      <h1>Employee Management Software</h1>
+      <div>
+        <button className='round-button' onClick={()=>setIsAdding(true)}>Add Employee</button>
+      </div>
+    </header>
   )
 }
 
--- src/Page/Dashboard/index.js
+++ src/Page/Dashboard/index.js
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Swal from "sweetalert2";
 import Header from "./Header";
 import List from "./List";
 import Add from "./Add";
 import Edit from "./Edit";
-import { employessData } from "../../data";
-function Dashboard() {
 
-  return <div>index</div>;
+import { employeesData } from "../../data";
+
+function Dashboard() {
+  //state start here
+  const [employees, setEmployees] = useState(employeesData);
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
+  const [isAdding, setIsAdding] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  //method of handleEdit
+  const handleEdit = () => {};
+
+  //method of handleDelete
+  const handleDelete = () => {};
+  return (
+    <div className="container">
+      {/* This is for List */}
+      {!isAdding && !isEditing && (
+        <>
+          <Header setIsAdding={setIsAdding} />
+          <List
+            employees={employees}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
+        </>
+      )}
+      {/* This is for Add */}
+      {isAdding && (
+        <Add
+          employees={employees}
+          setEmployees={setEmployees}
+          setIsEditing={setIsEditing}
+        />
+      )}
+      {/* This is for Editing */}
+      {isEditing && (
+        <Edit
+          employees={employees}
+          setEmployees={setEmployees}
+          selectedEmployee={selectedEmployee}
+          setIsAdding={setIsAdding}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Dashboard;
